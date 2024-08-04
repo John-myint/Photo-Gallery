03f187b2c95f4aebbeb0a887b76cf70dc06a306f
John-myint <kyawlaymyint94.mdy@gmail.com>
2024-08-04 22:15:07 +0700

    Decrease time properply

--- Alarm/main.js
+++ Alarm/main.js
@@ -3,7 +3,10 @@ const hr = document.getElementsByClassName("hr")[0];
 const min = document.getElementsByClassName("min")[0];
 const sec = document.getElementsByClassName("sec")[0];
 const pausebtn = document.getElementsByClassName("pausebtn")[0];
-const startbtn = document.getElementsByClassName("startbtn")[0];
+const hrInput = document.getElementsByClassName("input")[0];
+const minInput = document.getElementsByClassName("input")[1];
+const secInput = document.getElementsByClassName("input")[2];
+
 
 const hrDisplay = document.getElementsByClassName("hrDisplay")[0];
 const minDisplay = document.getElementsByClassName("minDisplay")[0];
@@ -13,7 +16,14 @@ let hrValue;
 let minValue;
 let secValue;
 let intervalId;
-const updateAlarm = () => {
+
+const resetInputValue = ()=> {
+    hrInput.value = "";
+    minInput.value = "";
+    secInput.value = "";
+}
+
+const updateAlarmDisplay = () => {
     hrDisplay.innerHTML = hrValue < 10 ? "0"+ hrValue : hrValue;
     minDisplay.innerHTML = minValue < 10 ? "0"+ minValue : minValue;
     secDisplay.innerHTML =secValue < 10 ? "0"+ secValue : secValue;
@@ -21,20 +31,35 @@ const updateAlarm = () => {
 }
 
 const executeAlarm = ()=> {
-    intervalId = setInterval(() => {
-        secValue === 0 ? secValue = 59 : secValue -= 1;
-        minValue === 0 ? minValue = 59 : 
-        updateAlarm();
-    }, 1000);
+    if (!intervalId) {
+        intervalId = setInterval(() => {
+            if (secValue === 0) {
+                if (minValue === 0) {
+                    if (hrValue === 0) {
+                        clearInterval(intervalId);
+                        intervalId = null;
+                        alert("Time's up..!")
+                    } else {
+                        hrValue -=1;
+                        minValue = 59;
+                    }
+                } else {
+                    minValue -=1;
+                }
+                secValue = 59;
+            } else {
+                secValue -=1;
+            }
+            updateAlarmDisplay();
+        }, 1000);
+    }
 }
 btn.addEventListener("click", ()=>{
-    hrValue = parseInt(hr.value , 10);
-    minValue = parseInt(min.value , 10);
-    secValue = parseInt(sec.value , 10);
-    updateAlarm();
-})
-
-startbtn.addEventListener("click",()=>{
+    hrValue = parseInt(hrInput.value , 10);
+    minValue = parseInt(minInput.value , 10);
+    secValue = parseInt(secInput.value , 10);
+    updateAlarmDisplay();
+    resetInputValue();
     executeAlarm();
 })
 
@@ -42,4 +67,6 @@ pausebtn.addEventListener("click", ()=>{
     clearInterval(intervalId);
     intervalId = null;
 })
+
+
 
